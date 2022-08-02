1ea60f2f1ebe52d8cf08d31b56fcecb5dc1ea9ad
Indriani Savitri <80106073+indri-vivi@users.noreply.github.com>
2022-08-02 23:19:23 +0800

    Update hrm-dashboard.py

--- Final Project/hrm-dashboard.py
+++ Final Project/hrm-dashboard.py
@@ -20,7 +20,40 @@ class Dashboard(unittest.TestCase):
         driver.find_element(By.ID,"txtPassword").send_keys("admin123") # fill password
         time.sleep(2)
         driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[5]/input").click() #click the login button
-        time.sleep(2) #1st login is automatically dashboard
+        time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/ul/li[8]/a").click() #open the dashboard
+        time.sleep (1)
 
+    def test_TC_031(self): #Check the Pie chart Information 
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com") # open the site
+        driver.find_element(By.ID,"txtUsername").send_keys("Admin") # fill username
+        time.sleep(2)
+        driver.find_element(By.ID,"txtPassword").send_keys("admin123") # fill password
+        time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[5]/input").click() #click the login button
+        time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/ul/li[8]/a").click() #open the dashboard
+        time.sleep (2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div[2]/div/div/div[1]/fieldset/div/div[1]/span[1]").click() #open the pie chart
+        time.sleep (2)
+
+    def test_TC_032(self): #Check the Shortcut for Leave List 
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com") # open the site
+        driver.find_element(By.ID,"txtUsername").send_keys("Admin") # fill username
+        time.sleep(2)
+        driver.find_element(By.ID,"txtPassword").send_keys("admin123") # fill password
+        time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[2]/div[2]/form/div[5]/input").click() #click the login button
+        time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/ul/li[8]/a").click() #open the dashboard
+        time.sleep (2)
+        driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div/div/fieldset/div/div/table/tbody/tr/td[2]/div/a/img").click() #open the leave list
+        time.sleep (2)
+
+    def tearDown(self): 
+        self.driver.close()
+        
 if __name__ == "__main__": 
     unittest.main()
